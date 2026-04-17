141b7ca46cb74d1b04b900001e2c46ed498cfd3a
dnnnt <joaquim_marcondes@outlook.com>
2026-04-17 15:11:41 -0300

    Update Vaga.java

--- src/main/java/com/projeto/webservice/model/Vaga.java
+++ src/main/java/com/projeto/webservice/model/Vaga.java
@@ -1,13 +1,21 @@
 package com.projeto.webservice.model;
+
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
+@Entity
 public class Vaga {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String descricao;
     private LocalDate publicacao;
     private boolean ativo;
     private Long idEmpresa;
+
+    public Vaga() {}
 
     public Vaga(Long id, String titulo, String descricao, LocalDate publicacao, boolean ativo, Long idEmpresa) {
         this.id = id;
@@ -20,20 +28,14 @@ public class Vaga {
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getTitulo() { return titulo; }
     public void setTitulo(String titulo) { this.titulo = titulo; }
-
     public String getDescricao() { return descricao; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
-
     public LocalDate getPublicacao() { return publicacao; }
     public void setPublicacao(LocalDate publicacao) { this.publicacao = publicacao; }
-
     public boolean isAtivo() { return ativo; }
     public void setAtivo(boolean ativo) { this.ativo = ativo; }
-
     public Long getIdEmpresa() { return idEmpresa; }
     public void setIdEmpresa(Long idEmpresa) { this.idEmpresa = idEmpresa; }
-
 }
