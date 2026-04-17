c46cfc66a5c13b458432fdf6e495aa51d69a736b
dnnnt <joaquim_marcondes@outlook.com>
2026-04-17 15:11:11 -0300

    Update Empresa.java

--- src/main/java/com/projeto/webservice/model/Empresa.java
+++ src/main/java/com/projeto/webservice/model/Empresa.java
@@ -1,10 +1,18 @@
 package com.projeto.webservice.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Empresa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String cnpj;
     private String emailContato;
+
+    public Empresa() {}
 
     public Empresa(Long id, String nome, String cnpj, String emailContato) {
         this.id = id;
@@ -15,14 +23,10 @@ public class Empresa {
     
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-
     public String getCnpj() { return cnpj; }
     public void setCnpj(String cnpj) { this.cnpj = cnpj; }
-
     public String getEmailContato() { return emailContato; }
     public void setEmailContato(String emailContato) { this.emailContato = emailContato; }
-    
 }
